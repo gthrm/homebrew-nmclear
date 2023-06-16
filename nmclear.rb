64080a164d3266a7ca7f3a9aaa7ac98c796bec6e
Roman Meshcheriakov <rmeshcheriakov@apple.com>
2023-06-16 21:23:30 +0200

    Update nmclear.rb

--- nmclear.rb
+++ nmclear.rb
@@ -1,7 +1,7 @@
 class Nmclear < Formula
     desc "My script that deletes node_modules directories"
     homepage "https://github.com/gthrm/homebrew-nmclear"
-    url "https://github.com/gthrm/homebrew-nmclear/archive/refs/tags/v1.0.0.tar.gz"
+    url "https://github.com/gthrm/homebrew-nmclear/archive/refs/tags/v1.0.2.tar.gz"
     sha256 "6fa39b8366f2d0297721008c809a5b7e4ab9a96e89b11d3496d6c24aec4db539"
   
     def install
