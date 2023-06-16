317037e21f435b967176bfaa1f75da2a49f9d175
Roman Meshcheriakov <rmeshcheriakov@apple.com>
2023-06-16 21:10:24 +0200

    Update url and sha256

--- nmclear.rb
+++ nmclear.rb
@@ -2,7 +2,7 @@ class Nmclear < Formula
     desc "My script that deletes node_modules directories"
     homepage "https://github.com/gthrm/homebrew-nmclear"
     url "https://github.com/gthrm/homebrew-nmclear/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "put_the_sha256_of_your_tarball_here"
+    sha256 "6fa39b8366f2d0297721008c809a5b7e4ab9a96e89b11d3496d6c24aec4db539"
   
     def install
       bin.install "nmclear"
